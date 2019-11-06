047609c01a311407482d31e8db011f1300881b7d
JanKinCai <caizhengxin16@163.com>
2019-11-06 23:52:53 +0800

    style(*.py): update format

--- {{cookiecutter.project_name}}/tests/test_{{cookiecutter.project_slug}}.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author: JanKinCai
-# @Date:   2019-09-05 09:11:58
-# @Last Modified by:   caizhengxin@bolean.com.cn
-# @Last Modified time: 2019-09-05 09:11:58
--- {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/__main__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/__main__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from __future__ import print_function
 
 from {{ cookiecutter.project_slug }}.command import main
